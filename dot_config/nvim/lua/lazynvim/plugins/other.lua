fcccae70698dd0cf64041a081e8d914ebf395e8c
kenis <kenisdsg1108@gmail.com>
2025-05-07 16:39:33 +0800

    feat(nvim): new config

--- dot_config/nvim/lua/lazynvim/plugins/other.lua
+++ dot_config/nvim/lua/lazynvim/plugins/other.lua
@@ -2,9 +2,20 @@ return {
   -- themes
   {
     "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
+    end,
   },
 
   -- Highlight todo, notes, etc in comments
