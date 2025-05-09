da5ced31aa8a19ddf0d9ecfb4105cb1ef730e3f1
kenis <kenisdsg1108@gmail.com>
2025-05-09 11:17:25 +0800

    feat(nvim): new config

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -42,4 +42,4 @@ map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
 -- windows
 map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
 map("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
-map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true }
+map("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
