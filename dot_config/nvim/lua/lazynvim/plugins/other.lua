1911644c808528c5cbb717b7dc323aa6a39c349f
kenis <kenisdsg1108@gmail.com>
2025-05-09 13:48:00 +0800

    feat(nvim): new config

--- dot_config/nvim/lua/lazynvim/plugins/other.lua
+++ dot_config/nvim/lua/lazynvim/plugins/other.lua
@@ -26,44 +26,44 @@ return {
   },
 
   -- syntax highlighting. Highlight, edit, and navigate code
-  { 
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function () 
-      local configs = require("nvim-treesitter.configs")
+  -- { 
+  --   "nvim-treesitter/nvim-treesitter",
+  --   build = ":TSUpdate",
+  --   config = function () 
+  --     local configs = require("nvim-treesitter.configs")
   
-      configs.setup({
-        ensure_installed = { 
-          "bash",
-          "css",      
-          "diff",
-          "html",
-          "javascript",
-          "jsdoc",
-          "json",
-          "jsonc",
-          "lua",
-          "luadoc",
-          "markdown",
-          "markdown_inline",
-          "nix",
-          "powershell",
-          "python",
-          "scss",
-          "toml",
-          "tsx",
-          "typescript",
-          "vim",
-          "vimdoc",
-          "vue",
-          "xml",
-          "yaml",
-        },
-        auto_install = true,
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
-      })
-    end
-  }
+  --     configs.setup({
+  --       ensure_installed = { 
+  --         "bash",
+  --         "css",      
+  --         "diff",
+  --         "html",
+  --         "javascript",
+  --         "jsdoc",
+  --         "json",
+  --         "jsonc",
+  --         "lua",
+  --         "luadoc",
+  --         "markdown",
+  --         "markdown_inline",
+  --         "nix",
+  --         "powershell",
+  --         "python",
+  --         "scss",
+  --         "toml",
+  --         "tsx",
+  --         "typescript",
+  --         "vim",
+  --         "vimdoc",
+  --         "vue",
+  --         "xml",
+  --         "yaml",
+  --       },
+  --       auto_install = true,
+  --       sync_install = false,
+  --       highlight = { enable = true },
+  --       indent = { enable = true },  
+  --     })
+  --   end
+  -- }
 }
