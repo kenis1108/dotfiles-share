b3d86d2091e16b8ed7441fa55a773d99b7a3a13a
kenis <kenisdsg1108@gmail.com>
2025-05-26 14:11:44 +0800

    feat(nvim): update config

--- dot_config/nvim/lua/lazynvim/plugins/ui.lua
+++ dot_config/nvim/lua/lazynvim/plugins/ui.lua
@@ -111,14 +111,30 @@ return {
             padding = 1,
           },
           {
-            desc = "  Blog https://kenis1108.github.io",
-            gap = 1, padding = 1,
+            text = {
+              { "  ", hl = "SnacksDashboardIcon" },
+              { "Blog ", hl = "SnacksDashboardDesc" },
+              { "https://kenis1108.github.io", hl = "SnacksDashboardKey" },
+            },
+            gap = 1, 
+            padding = 1,
           },
           { section = "keys", gap = 1, padding = 1 },
           { icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
           { section = "startup" },
         },
         preset = {
+          header = [[
+            .-. .-')     ('-.       .-') _          .-')             ('-. .-.          ('-. .-. 
+            \  ( OO )  _(  OO)     ( OO ) )        ( OO ).          ( OO )  /         ( OO )  / 
+            ,--. ,--. (,------.,--./ ,--,' ,-.-') (_)---\_)         ,--. ,--.     ,--.,--. ,--. 
+            |  .'   /  |  .---'|   \ |  |\ |  |OO)/    _ |    .-')  |  | |  | .-')| ,||  | |  | 
+            |      /,  |  |    |    \|  | )|  |  \\  :` `.  _(  OO) |   .|  |( OO |(_||   .|  | 
+            |     ' _)(|  '--. |  .     |/ |  |(_/ '..`''.)(,------.|       || `-'|  ||       | 
+            |  .   \   |  .--' |  |\    | ,|  |_.'.-._)   \ '------'|  .-.  |,--. |  ||  .-.  | 
+            |  |\   \  |  `---.|  | \   |(_|  |   \       /         |  | |  ||  '-'  /|  | |  | 
+            `--' '--'  `------'`--'  `--'  `--'    `-----'          `--' `--' `-----' `--' `--' 
+          ]],
           keys = {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
